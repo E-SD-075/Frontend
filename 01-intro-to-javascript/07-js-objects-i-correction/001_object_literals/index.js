3dfcd713b14e4beeadf25f90a212c843bc93706c
Garrett Lander <garrett.jlander@gmail.com>
2026-05-11 15:05:36 +0200

    js object i correction

--- 01-intro-to-javascript/07-js-objects-i-correction/001_object_literals/index.js
+++ 01-intro-to-javascript/07-js-objects-i-correction/001_object_literals/index.js
@@ -1,8 +1,29 @@
 // 1.  Create an object called `book` using object literal syntax.
+
 // 2.  The `book` object should have the following properties:
-//     *   `title`: a string representing the title of the book.
-//     *   `author`: a string representing the author of the book.
-//     *   `pages`: a number representing the total pages in the book.
-//     *   `isRead`: a boolean indicating if the book has been read or not.
-// 3.  Add a method named `summary` to the `book` object. This method should return a string summarizing the book details in the format:
-//     *   `"Title: [title], Author: [author], Pages: [pages], Read: [Yes/No]"`.
+const book = {
+	//     *   `title`: a string representing the title of the book.
+	title: 'Future Plan',
+	//     *   `author`: a string representing the author of the book.
+	author: 'Community',
+	//     *   `pages`: a number representing the total pages in the book.
+	pages: 55,
+	//     *   `isRead`: a boolean indicating if the book has been read or not.
+	isRead: true,
+	// 3.  Add a method named `summary` to the `book` object. This method should return a string summarizing the book details in the format:
+	//     *   `"Title: [title], Author: [author], Pages: [pages], Read: [Yes/No]"`.
+	summaryOfTheBook() {
+		// let readStr;
+		// if (this.isRead) {
+		// 	readStr = 'Yes';
+		// } else {
+		// 	readStr = 'No';
+		// }
+
+		// return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Read: ${readStr}`;
+		return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Read: ${this.isRead ? 'Yes' : 'No'}`;
+	}
+};
+
+console.log(book);
+console.log(book.summaryOfTheBook());
--- 01-intro-to-javascript/07-js-objects-i-correction/002_destructuring/index.js
+++ 01-intro-to-javascript/07-js-objects-i-correction/002_destructuring/index.js
@@ -2,11 +2,17 @@
 // Initial array
 const fruits = ['apple', 'banana', 'cherry', 'date'];
 
-//     Extract the first two elements from the `fruits` array and store them in variables `fruit1` and `fruit2`.
+//     Extract the first two elements from the `fruits` array and store them in
+// variables `fruit1` and `fruit2`.
+const [fruit1, fruit2] = fruits;
+console.log('fruit1, fruit2: ', fruit1, fruit2);
 
 // *   **Skipping Elements in Array Destructuring**
 
 //     Extract the first and third elements from the `fruits` array, skipping the second element.
+const [first, , third] = fruits;
+
+console.log('first, third:', first, third);
 
 // *   **Simple Object Destructuring**
 // Initial object
@@ -20,16 +26,40 @@ const person = {
 };
 
 //     Extract the `name` and `age` properties from the `person` object.
+const { name, age } = person;
+console.log('name, age:', name, age);
 
 // *   **Nested Object Destructuring**
 
 //     Extract the `city` from the `address` property of the `person` object.
+const {
+	address: { city }
+} = person;
+console.log('city:', city);
+// console.log(address); // uncaught reference error
 
 // *   **Destructuring in Function Parameters**
 //     `displayPerson` is function that takes a `person` object
 // and logs the `name` and `age` properties using dot notation.
 // Modify the function to destructure the `name` and `age` properties directly in the parameters.
 // Initial function
-function displayPerson(person) {
-	console.log(`Name: ${person.name}, Age: ${person.age}`);
+// function displayPerson(person) {
+// 	console.log(`Name: ${person.name}, Age: ${person.age}`);
+// }
+function displayPerson({ name, age }) {
+	console.log(`Name: ${name}, Age: ${age}`);
 }
+
+displayPerson(person);
+
+function exampleFunc({ param1, param2, param3 }) {
+	console.log(param1, param2, param3);
+}
+const exampleObj = {
+	param1: "I'm first!",
+	param2: "I'm second",
+	param3: "I'm third!"
+};
+
+// exampleFunc("I'm second", "I'm first", "I'm third");
+exampleFunc(exampleObj);
