e2a2571c2448d3e30abf9663f3f3e428e8e318ac
Garrett Lander <garrett.jlander@gmail.com>
2026-05-22 09:57:35 +0200

    globals correction

--- 03-js-modules/01-setTimeout-setInterval-correction/004_schedule_a_fetch_request/index.js
+++ 03-js-modules/01-setTimeout-setInterval-correction/004_schedule_a_fetch_request/index.js
@@ -1,14 +1,69 @@
 // Schedule a fetch request
 // ========================
 
-// **Objective:** Use `setInterval` to schedule a fetch request to the Pokémon API and increment a counter. For every request, output an object with the name of the Pokémon and its ID.
+// **Objective:** Use `setInterval` to schedule a fetch request to the Pokémon API and increment a counter. For every request,
+// output an object with the name of the Pokémon and its ID.
 
 // **Instructions:**
 
 // *   Create a variable `counter` and set it to 1.
+// let counter = 1;
+// // *   Use `setInterval` to schedule a fetch request to the Pokémon API every second.
+// const intervalId = setInterval(() => {
+// 	// *   Stop the interval after 150 requests. We only want the OG Pokémon! :D
+// 	if (counter > 150) {
+// 		clearInterval(intervalId);
+// 		return;
+// 	}
+// 	// *   The fetch request URL should be `https://pokeapi.co/api/v2/pokemon/` followed by the current value of `counter`.
+// 	const url = `https://pokeapi.co/api/v2/pokemon/${counter}`;
+// 	// *   Fetch the data and extract the Pokémon's name and ID.
+// 	fetch(url)
+// 		.then((response) => response.json())
+// 		.then((data) => {
+// 			const pokemonInfo = {
+// 				name: data.name,
+// 				id: data.id
+// 			};
+// 			// *   Output an object with the Pokémon's name and ID to the console.
+// 			console.log(pokemonInfo);
+// 		})
+// 		.catch((error) => console.error(error));
+// 	// *   Increment the counter after each fetch request.
+// 	counter++;
+// }, 1000);
+const getPokemon = async (pokemonId) => {
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+	if (!res.ok) throw new Error(`${res.status}. Something went wrong!`);
+
+	const data = await res.json();
+
+	return data;
+};
+let counter = 1;
 // *   Use `setInterval` to schedule a fetch request to the Pokémon API every second.
-// *   The fetch request URL should be `https://pokeapi.co/api/v2/pokemon/` followed by the current value of `counter`.
-// *   Fetch the data and extract the Pokémon's name and ID.
-// *   Output an object with the Pokémon's name and ID to the console.
-// *   Increment the counter after each fetch request.
-// *   Stop the interval after 150 requests. We only want the OG Pokémon! :D
+const intervalId = setInterval(async () => {
+	// *   The fetch request URL should be `https://pokeapi.co/api/v2/pokemon/` followed by the current value of `counter`.
+	const { name, id } = await getPokemon(counter);
+	// *   Fetch the data and extract the Pokémon's name and ID.
+	// const pokemon = { name: name, id: id };
+	// shorthand for same meaning as line above
+	const pokemon = { name, id };
+	console.log(pokemon);
+	// *   Increment the counter after each fetch request.
+	counter++;
+	// *   Stop the interval after 150 requests. We only want the OG Pokémon! :D
+	if (counter > 150) clearInterval(intervalId);
+}, 1000);
+
+// demo of argument not updating variable - pass by value
+// let counter = 1;
+
+// const incrementCounter = (counterArg) => {
+// 	counterArg++;
+// 	console.log('counterArg:', counterArg);
+// };
+
+// incrementCounter(counter);
+
+// console.log('counter after:', counter);
--- 03-js-modules/01-setTimeout-setInterval-correction/005_schedule_the_creation_of_a_dom_element/script.js
+++ 03-js-modules/01-setTimeout-setInterval-correction/005_schedule_the_creation_of_a_dom_element/script.js
@@ -1,14 +1,28 @@
-// Schedule the creation of a DOM element
-// ======================================
-
-// **Objective:**
-
-// Use `setTimeout` to schedule the creation of a DOM element that displays a promotion message styled with Tailwind CSS.
-
-// **Instructions:**
-
 // *   Use `setTimeout` to schedule the creation of a promotion message after 3 seconds.
-// *   Create a new DOM element (e.g., a `<div>`) for the promotion message.
-// *   Add some text to the promotion message, like "Special Offer: Get 20% off your next purchase!".
-// *   Use Tailwind CSS to style the message with a background color, padding, rounded corners, and centered text.
-// *   Append the new element to the `message-container` div.
+// const showMessage = () => {
+// 	const messageContainer = document.querySelector('#message-container');
+// 	// *   Add some text to the promotion message, like "Special Offer: Get 20% off your next purchase!".
+// 	let textNode = document.createTextNode(
+// 		'Special Offer: Get 20% off your next purchase!'
+// 	);
+// 	// *   Create a new DOM element (e.g., a `<div>`) for the promotion message.
+// 	let divContainer = document.createElement('div');
+// 	divContainer.appendChild(textNode);
+// 	// *   Use Tailwind CSS to style the message with a background color, padding, rounded corners, and centered text.
+// 	divContainer.classList =
+// 		'mt-5 px-4 py-2 bg-red-500 hover:bg-blue-400 text-white rounded';
+// 	// *   Append the new element to the `message-container` div.
+// 	messageContainer.appendChild(divContainer);
+// };
+// setTimeout(showMessage, 3000);
+
+const messageContainer = document.querySelector('#message-container');
+
+setTimeout(() => {
+	const h2 = document.createElement('h2');
+	h2.textContent = 'Special Offer: Get 20% off your next purchase!';
+	h2.className =
+		'bg-red-500 text-white p-4 rounded-lg shadow-lg max-w-md mx-auto';
+
+	messageContainer.appendChild(h2);
+}, 3000);
